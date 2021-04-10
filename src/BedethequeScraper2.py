896be4f57150849d2af33a31ce2843eef626f350
maforget <11904426+maforget@users.noreply.github.com>
2021-04-10 14:18:55 -0400

    Forgot to update file to v5.4

--- src/BedethequeScraper2.py
+++ src/BedethequeScraper2.py
@@ -50,7 +50,7 @@ BasicXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><configuration></configura
 
 CookieContainer = System.Net.CookieContainer()
 
-VERSION = "5.3"
+VERSION = "5.4"
 
 SHOWRENLOG = False
 SHOWDBGLOG = False
