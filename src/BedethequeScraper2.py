084454df567c1048a1e23fc9e207b70c48ebf6dd
maforget <11904426+maforget@users.noreply.github.com>
2022-05-29 16:05:43 -0400

    Package v5.12

--- src/BedethequeScraper2.py
+++ src/BedethequeScraper2.py
@@ -50,7 +50,7 @@ BasicXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><configuration></configura
 
 CookieContainer = System.Net.CookieContainer()
 
-VERSION = "5.11"
+VERSION = "5.12"
 
 SHOWRENLOG = False
 SHOWDBGLOG = False
