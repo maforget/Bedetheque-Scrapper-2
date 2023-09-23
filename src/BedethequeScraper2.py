64ac4849e6d28dcf0855e925623d3c587d69cd0f
maforget <11904426+maforget@users.noreply.github.com>
2023-09-22 21:23:54 -0400

    upgrade to v5.16

--- src/BedethequeScraper2.py
+++ src/BedethequeScraper2.py
@@ -50,7 +50,7 @@ BasicXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><configuration></configura
 
 CookieContainer = System.Net.CookieContainer()
 
-VERSION = "5.15 Beta1"
+VERSION = "5.16"
 
 SHOWRENLOG = False
 SHOWDBGLOG = False
